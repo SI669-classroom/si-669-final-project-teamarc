a579488c11d6cacee5becd91fd89c50af4758d27
Ceciel Zhong <xizhong@umich.edu>
2023-12-06 01:11:28 -0500

    Updated version and layout; still need to make it so that button can be accessed after scrolling

--- screens/TicTacToeScreen.js
+++ screens/TicTacToeScreen.js
@@ -12,7 +12,12 @@ import LogoImage from '../components/LogoImage.js'
 import { getAuthUser } from "../AuthManager.js";
 import { addGame } from "../data/Actions.js";
 import Square from "../components/Square.js";
+import { serverTimestamp } from "firebase/firestore";
 
+// Todo:
+// - Stop the game if there is no winner and all squares are filled
+// - Send status to firebase after each move
+// - Load status from firebase on start
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
@@ -52,15 +57,15 @@ function Board({ xIsNext, squares, onPlay }) {
       </View> 
 
       <View style={styles.boardRowAfter}>
-        <Square value={squares[3]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </View> 
 
       <View style={styles.boardRowAfter}>
-        <Square value={squares[6]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </View>  
     </View> 
   );
@@ -77,8 +82,6 @@ function TicTacToeScreen(props) {
   const dispatch = useDispatch();
   const myKey = getAuthUser().uid;
 
-
-
   const [history, setHistory] = useState([Array(9).fill(null)]); //initialize the board with 9 empty grids
   const [currentMove, setCurrentMove] = useState(0);//use this to set where this move is placed
   const xIsNext = currentMove % 2 === 0; //indicator of which is next (x or o)
@@ -93,6 +96,34 @@ function TicTacToeScreen(props) {
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
+
+  // const moves = history.map((squares, move) => {
+  //   let description;
+  //   if (move > 0) {
+  //     description = 'Go to move #' + move;
+  //   } else {
+  //     description = 'Go to game start';
+  //   }
+  //   return (
+  //     <li key={move}>
+  //       <button onClick={() => jumpTo(move)}>{description}</button>
+  //     </li>
+  //   );
+  // }); 
+
+  const moves = ({ item, index }) => {
+    const move = index;
+    const description = move ?
+      `Go to move #${move}` :
+      'Go to game start';
+    return (
+      <View style={styles.listItem}>
+        <Text style={styles.listItemText} onPress={() => jumpTo(move)}>
+          {description}
+        </Text>
+      </View>
+    );
+  };
 
 
   return (
@@ -109,39 +140,15 @@ function TicTacToeScreen(props) {
 
         <View style={styles.game}>
           <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} /> 
+          <FlatList
+            data={history}
+            renderItem={moves}
+            keyExtractor={(item, index) => index.toString()}
+            style={styles.movesList}
+          /> 
+          {/* game-info was the style for these buttons */} 
+        </View> 
 
-          {/* <ol>{moves}</ol> 
-          const moves = history.map((squares, move) => {
-            let description;
-            if (move > 0) {
-              description = 'Go to move #' + move;
-            } else {
-              description = 'Go to game start';
-            }
-            return (
-              <li key={move}>
-                <button onClick={() => jumpTo(move)}>{description}</button>
-              </li>
-            );
-          }); */}
-
-
-          {/* <FlatList
-            data={moves}
-            renderItem={({item}) => {
-              return (
-                <View>
-                  <Button>{item.type}</Button>
-                </View>
-              );
-            }}
-          /> */}
-
-          {/* game-info is the style */}
-
-
-        </View>
-        
         <View style={styles.allButtons}>
           <Button
             color="#FFD600"
@@ -241,62 +248,34 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
 
-
   //styles for the board itself
-  square: {
-    background: '#fff',
-    border: '1px solid #999',
-    float: 'left',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    lineHeight: '34px',
-    height: '34px',
-    marginRight: '-1px',
-    marginTop: '-1px',
-    padding: '0',
-    textAlign: 'center',
-    width: '34px', 
-  },
-
-  
-  game: {
-    // flex: .8,
-    // width: '90%',
-    // height: '70%',
-    // justifyContent: 'center',
-    // alignItems:'center',  
-    background: '#fff',
-    border: '1px solid #999',
-    float: 'left',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    lineHeight: '34px',
-    height: '34px',
-    marginRight: '-1px',
-    marginTop: '-1px',
-    padding: '0',
-    textAlign: 'center',
-    width: '34px',
-  },
-
   boardRowAfter: {
-
-    clear: 'both',
-    content: '',
-    display: 'table',
+    flexDirection: 'row',       // align children horizontally
+    justifyContent: 'center',   // center children in the row
+    alignItems: 'center',       // align items in the center of the cross-axis
   },
   status:{
-    marginBottom: '10px',
+    marginBottom: '10',
   },
 
-  game:{
+  game:{ //game contains the board and the list of moves
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
+    flex:1,
+  },
+
+  movesList:{
+    flex:1,
   },
 
   gameInfo:{
-    marginLeft: '20px',
+    marginLeft: '20',
+  },
+
+  footer:{
+    padding:1
   }
+ 
 });
 
 export default TicTacToeScreen;
