a515db968d2873a11dfe17282a8c7edda950a442
emilycao316 <emilycao@umich.edu>
2023-12-14 08:36:59 -0500

    fix placement of files

--- components/FigureMan.js
+++ components/FigureMan.js
@@ -6,13 +6,13 @@ import { useSelector, useDispatch } from "react-redux";
 function FigureMan(props) {
     const { wrongWord } = props;
     // console.log(wrongWord)
-    const Rope = <Image style={styles.image} source={require('../../images/Rope.png')} />
-    const Head = <Image style={styles.image} source={require('../../images/Head.png')} />
-    const Body = <Image style={styles.image} source={require('../../images/Body.png')} />
-    const LeftArm = <Image style={styles.image} source={require('../../images/LeftArm.png')} />
-    const RightArm = <Image style={styles.image} source={require('../../images/RightArm.png')} />
-    const LeftLeg = <Image style={styles.image} source={require('../../images/LeftLeg.png')} />
-    const RightLeg = <Image style={styles.image} source={require('../../images/RightLeg.png')} />
+    const Rope = <Image style={styles.image} source={require('../images/Rope.png')} />
+    const Head = <Image style={styles.image} source={require('../images/Head.png')} />
+    const Body = <Image style={styles.image} source={require('../images/Body.png')} />
+    const LeftArm = <Image style={styles.image} source={require('../images/LeftArm.png')} />
+    const RightArm = <Image style={styles.image} source={require('../images/RightArm.png')} />
+    const LeftLeg = <Image style={styles.image} source={require('../images/LeftLeg.png')} />
+    const RightLeg = <Image style={styles.image} source={require('../images/RightLeg.png')} />
     const bodyParts = [Rope,Head,Body, LeftArm, RightArm, LeftLeg, RightLeg]
     
   return (
